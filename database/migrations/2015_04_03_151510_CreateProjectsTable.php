a08433470ebbb988c0f826fe6445ea6aa4818d33
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2015-04-03 13:32:20 -0400

    added migrations for projects and models

---
+++ database/migrations/2015_04_03_151510_CreateProjectsTable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('projects', function(Blueprint $table)
+		{
+			$table->increments('pid');
+			$table->integer('nextModel')->unsigned();
+			$table->string('name');
+			$table->string('slug')->unique();
+			$table->string('description');
+			$table->integer('adminId')->unsigned();
+			$table->boolean('active');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('projects');
+	}
+
+}
