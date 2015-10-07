c95cbd0fd3d02820b67980d45e75c7d2da06d0ee
Ian Whalen <ian.whalen@peach.matrix.msu.edu>
2015-10-07 15:25:28 -0400

    Small changes to quick jump.

--- resources/views/partials/menu/form.blade.php
+++ resources/views/partials/menu/form.blade.php
@@ -2,15 +2,18 @@
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ \App\Http\Controllers\FormController::getForm($fid)->name }}<b class="caret"></b></a>
     <ul class="dropdown-menu">
         <li><a href="{{ url('/projects/'.$pid).'/forms/'.$fid}}">Form Layout</a></li>
-        <li class="dropdown-submenu"> <a href="#" data-toggle="dropdown">Jump to Form</a>
-            <ul class="dropdown-menu">
-                @foreach(\Auth::user()->allowedForms($pid) as $form)
-                    @if($form->fid != $fid)
-                        <li><a href="{{ url('/projects/'.$pid).'/forms/'.$form->fid }}">{{ $form->name }}</a></li>
-                    @endif
-                @endforeach
-            </ul>
-        </li>
+        <?php $allowed_forms = \Auth::user()->allowedForms($pid) ?>
+        @if(sizeof($allowed_forms) > 1)
+            <li class="dropdown-submenu"> <a href="#" data-toggle="dropdown">Jump to Form</a>
+                <ul class="dropdown-menu">
+                    @foreach($allowed_forms as $form)
+                        @if($form->fid != $fid)
+                            <li><a href="{{ url('/projects/'.$pid).'/forms/'.$form->fid }}">{{ $form->name }}</a></li>
+                        @endif
+                    @endforeach
+                </ul>
+            </li>
+        @endif
         <li class="divider"></li>
         <li><a href="{{ url('/projects/'.$pid).'/forms/'.$fid.'/records'}}">Records</a></li>
         <li><a href="{{ url('/projects/'.$pid).'/forms/'.$fid.'/records/create'}}">New Record</a></li>
--- resources/views/partials/menu/project.blade.php
+++ resources/views/partials/menu/project.blade.php
@@ -2,14 +2,17 @@
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ \App\Http\Controllers\ProjectController::getProject($pid)->name }}<b class="caret"></b></a>
     <ul class="dropdown-menu">
         <li><a href="{{ url('/projects/'.$pid) }}">Project Home</a></li>
-        <li class="dropdown-submenu"> <a onmouseover="href='#'" data-toggle="dropdown">Jump to Project</a>
-            <ul class="dropdown-menu">
-                @foreach(\Auth::user()->allowedProjects() as $project)
-                    @if($project->pid != $pid)
-                        <li><a href="{{ url('/projects/'.$project->pid) }}">{{ $project->name }}</a></li>
-                    @endif
-                @endforeach
-            </ul>
-        </li>
+        <?php $allowed_projects = \Auth::user()->allowedProjects() ?>
+        @if(sizeof($allowed_projects) > 1)
+            <li class="dropdown-submenu"> <a onmouseover="href='#'" data-toggle="dropdown">Jump to Project</a>
+                <ul class="dropdown-menu">
+                    @foreach($allowed_projects as $project)
+                        @if($project->pid != $pid)
+                            <li><a href="{{ url('/projects/'.$project->pid) }}">{{ $project->name }}</a></li>
+                        @endif
+                    @endforeach
+                </ul>
+            </li>
+        @endif
     </ul>
 </li>
