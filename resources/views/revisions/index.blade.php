3167f724a335a9f59ca4c2f7ab1d160a1553358c
Ian Whalen <ian.whalen@peach.matrix.msu.edu>
2016-02-02 10:32:37 -0500

    Fixed a problem with rolling back numberfields. Added a loading animation when a record is being rolled back.

--- resources/views/revisions/index.blade.php
+++ resources/views/revisions/index.blade.php
@@ -28,14 +28,25 @@
                         {!! Form::label('search', trans('revisions_index.search').': ') !!}
                         {!! Form::select('search', $records, ['class'=>'form-control']) !!}
                         <button class="btn btn-primary" onclick="showRecordRevisions(1, '')">{{trans('revisions_index.showrev')}}</button>
+
                         @if($message != 'Recent')
-                        <button class="btn btn-primary" onclick="showRecordRevisions(-1, '')">{{trans('revisions_index.back')}}</button>
+                            <button class="btn btn-primary" onclick="showRecordRevisions(-1, '')">{{trans('revisions_index.back')}}</button>
                         @endif
+
                         <hr/>
 
-                        @include('revisions.printrevisions')
+                        <div id="revisions">
+                            @include('revisions.printrevisions')
+                        </div>
+
+                        <div style="display:none; margin-top: 1em;" id="progress" class="progress">
+                            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                                {{trans('update_index.loading')}}
+                            </div>
+                        </div>
 
                     </div>
+
                 </div>
             </div>
         </div>
@@ -63,6 +74,9 @@
     function rollback(revision) {
         resp = confirm('{{trans('revisions_index.areyousure')}}?');
         if(resp) {
+
+            showProgress();
+
             $.ajax({
               url: '{{action('RevisionController@rollback')}}',
               type: 'GET',
@@ -75,6 +89,14 @@
               }
             });
         }
+    }
+
+    function showProgress() {
+        var revisions = $("#revisions");
+        revisions.slideUp();
+
+        var progress = $("#progress");
+        progress.css("display", "");
     }
 
     $( ".panel-heading" ).on( "click", function() {
