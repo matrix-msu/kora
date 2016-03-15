210f31486c5bd1f226aa45f2d90683cf333e46cf
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2016-03-15 11:06:53 -0400

    Adding what I have for csv export. Will return to import/export after field restructures

--- resources/lang/es/partials_menu_form.php
+++ resources/lang/es/partials_menu_form.php
@@ -8,6 +8,7 @@ return [
 	"jump" => "Saltar al formulario",
 	"records" => "Registros",
 	"newrec" => "Nuevo registro",
+    "import" => "Archivos de importaci&oacute;n",
 	"massassign" => "Asignaci&oacute;n masiva de registros",
 	"lod" => "Linked Open Data",
 	"groups" => "Administrar grupos",
