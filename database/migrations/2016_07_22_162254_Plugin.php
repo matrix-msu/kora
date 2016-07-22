628b21fa3dee7a056a4cc252c61a56738c72ac74
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2016-07-22 14:33:40 -0400

    Added table layouts for plugins. Added test layout for plugin manager page

--- database/migrations/2016_07_22_162254_Plugin.php
+++ database/migrations/2016_07_22_162254_Plugin.php
@@ -12,7 +12,54 @@ class Plugin extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('plugins', function(Blueprint $table)
+        {
+            $table->engine = 'MyISAM';
+
+            $table->increments('id');
+            $table->string('name');
+            $table->boolean('active');
+            $table->string('url');
+            $table->timestamps();
+
+        });
+
+        Schema::create('plugin_settings', function(Blueprint $table)
+        {
+            $table->engine = 'MyISAM';
+
+            $table->integer('plugin_id');
+            $table->string('option');
+            $table->string('value');
+            $table->timestamps();
+
+            $table->foreign('plugin_id')->references('id')->on('plugins')->onDelete('cascade');
+        });
+
+        Schema::create('plugin_users', function(Blueprint $table)
+        {
+            $table->engine = 'MyISAM';
+
+            $table->integer('plugin_id');
+            $table->integer('uid');
+            $table->timestamps();
+
+            $table->foreign('plugin_id')->references('id')->on('plugins')->onDelete('cascade');
+            $table->foreign('uid')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('plugin_menus', function(Blueprint $table)
+        {
+            $table->engine = 'MyISAM';
+
+            $table->integer('plugin_id');
+            $table->string('name');
+            $table->string('url');
+            $table->integer('order');
+            $table->timestamps();
+
+            $table->foreign('plugin_id')->references('id')->on('plugins')->onDelete('cascade');
+        });
     }
 
     /**
@@ -22,6 +69,9 @@ class Plugin extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('plugin_menus');
+        Schema::drop('plugin_users');
+        Schema::drop('plugin_settings');
+        Schema::drop('plugins');
     }
 }
