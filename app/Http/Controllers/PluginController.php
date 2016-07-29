25e22954c6363484962a985ec7d72fa444164c0c
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2016-07-29 11:28:56 -0400

    Plugin install process now install k3Form files

--- app/Http/Controllers/PluginController.php
+++ app/Http/Controllers/PluginController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Field;
+use App\Form;
+use App\FormGroup;
+use App\Metadata;
 use App\Plugin;
 use App\Project;
 use App\ProjectGroup;
+use App\RecordPreset;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -142,6 +147,13 @@ class PluginController extends Controller
         DB::insert("insert into ".env('DB_PREFIX')."plugin_users (plugin_id, gid) values (?, ?)",
             [$plugin->id, $proj->adminGID]);
 
+        //import forms associated with plugin
+        foreach (new \DirectoryIterator(env('BASE_PATH') . 'storage/app/plugins/'.$name.'/Forms/') as $file) {
+            if ($file->isFile()) {
+                $this->importForm($plugin->pid,env('BASE_PATH') . 'storage/app/plugins/'.$name.'/Forms/'.$file->getFilename());
+            }
+        }
+
         flash()->overlay(trans('controller_plugin.install'),trans('controller_plugin.goodjob'));
     }
 
@@ -197,5 +209,143 @@ class PluginController extends Controller
         $plugin = Plugin::where('id','=',$id)->first();
 
         return $plugin;
+    }
+
+    private function importForm($pid, $filepath){
+        $project = ProjectController::getProject($pid);
+
+        if(!\Auth::user()->admin && !\Auth::user()->isProjectAdmin($project)){
+            return redirect('projects/'.$pid);
+        }
+
+        $fileArray = json_decode(file_get_contents($filepath));
+
+        //dd($fileArray);
+
+        $form = new Form();
+
+        $form->pid = $project->pid;
+        $form->name = $fileArray->name;
+        if (Form::where('slug', '=', $fileArray->slug)->exists()) {
+            $unique = false;
+            $i=1;
+            while(!$unique){
+                if(Form::where('slug', '=', $fileArray->slug.$i)->exists()){
+                    $i++;
+                }else{
+                    $form->slug = $fileArray->slug.$i;
+                    $unique = true;
+                }
+            }
+        }else{
+            $form->slug = $fileArray->slug;
+        }
+        $form->description = $fileArray->desc;
+        $form->layout = $fileArray->layout;
+        $form->preset = $fileArray->preset;
+        $form->public_metadata = $fileArray->metadata;
+
+        $form->save();
+
+        //make admin group
+        $admin = $this->makeFormAdminGroup($form);
+        $form->adminGID = $admin->id;
+        $form->save();
+
+        //record presets
+        $recPresets = $fileArray->recPresets;
+
+        foreach($recPresets as $pre) {
+            $rec = new RecordPreset();
+
+            $rec->fid = $form->fid;
+            $rec->name = $pre->name;
+            $rec->preset = $pre->preset;
+
+            $rec->save();
+        }
+
+        $fields = $fileArray->fields;
+
+        foreach($fields as $fieldArray){
+            $field = new Field();
+
+            $field->pid = $project->pid;
+            $field->fid = $form->fid;
+            $field->type = $fieldArray->type;
+            $field->name = $fieldArray->name;
+            if (Field::where('slug', '=', $fieldArray->slug)->exists()) {
+                $unique = false;
+                $i=1;
+                while(!$unique){
+                    if(Field::where('slug', '=', $fieldArray->slug.$i)->exists()){
+                        $i++;
+                    }else{
+                        $field->slug = $fieldArray->slug.$i;
+                        $unique = true;
+                    }
+                }
+            }else{
+                $field->slug = $fieldArray->slug;
+            }
+            $field->desc = $fieldArray->desc;
+            $field->required = $fieldArray->required;
+            $field->default = $fieldArray->default;
+            $field->options = $fieldArray->options;
+
+            $field->save();
+
+            //fix layout
+            $form->layout = str_replace('<ID>'.$fieldArray->slug.'</ID>','<ID>'.$field->flid.'</ID>',$form->layout);
+            $form->save();
+
+            //metadata
+            if($fieldArray->metadata!=""){
+                $meta = new Metadata();
+                $meta->flid = $field->flid;
+                $meta->pid = $project->pid;
+                $meta->fid = $form->fid;
+                $meta->name = $fieldArray->metadata;
+                $meta->save();
+            }
+        }
+    }
+
+    private function makeFormAdminGroup(Form $form)
+    {
+        $groupName = $form->name;
+        $groupName .= ' Admin Group';
+
+        $adminGroup = new FormGroup();
+        $adminGroup->name = $groupName;
+        $adminGroup->fid = $form->fid;
+        $adminGroup->save();
+
+        $formProject = $form->project()->first();
+        $projectAdminGroup = $formProject->adminGroup()->first();
+
+        $projectAdmins = $projectAdminGroup->users()->get();
+        $idArray = [];
+
+        //Add all current project admins to the form's admin group.
+        foreach($projectAdmins as $projectAdmin)
+            $idArray[] .= $projectAdmin->id;
+
+
+        $idArray = array_unique(array_merge(array(\Auth::user()->id), $idArray));
+
+        if (!empty($idArray))
+            $adminGroup->users()->attach($idArray);
+
+        $adminGroup->create = 1;
+        $adminGroup->edit = 1;
+        $adminGroup->delete = 1;
+        $adminGroup->ingest = 1;
+        $adminGroup->modify = 1;
+        $adminGroup->destroy = 1;
+
+        $adminGroup->save();
+
+        return $adminGroup;
     }
 }
