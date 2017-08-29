d3220fe947b3396131e984075967e2139774d76b
Bogdan Pozderca <bogdanpozderca@gmail.com>
2017-08-29 14:54:47 -0400

    iOS bug test fix

--- public/assets/javascripts/general/global.js
+++ public/assets/javascripts/general/global.js
@@ -1,7 +1,23 @@
+var iOS = agent.indexOf('iphone') >= 0 || agent.indexOf('ipad') >= 0;
+var clickEvent = iOs ? 'touchend' : 'click';
+var touchMoving = false;
+
+if (iOS) {
+  document.ontouchmove = function(e) {
+    touchMoving = true;
+  }
+
+  document.ontouchend = function(e) {
+    touchMoving = false;
+  }
+}
+
 $(document).ready(function() {
   $('.underline-middle-hover, .underline-left-hover').on('click touchend', function(e) {
     var el = $(this);
     var link = el.attr('href');
+
+    if (touchMoving) return false;
 
     if (link.charAt(0) !== "#") {
       window.location = link;
