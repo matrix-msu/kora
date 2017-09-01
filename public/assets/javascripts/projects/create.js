4807d634244be317a682a146716658e649963e07
Bogdan Pozderca <bogdanpozderca@gmail.com>
2017-09-01 16:05:34 -0400

    Remove debugger

--- public/assets/javascripts/projects/create.js
+++ public/assets/javascripts/projects/create.js
@@ -6,11 +6,4 @@ Kora.Projects.Create = function() {
   $('.multi-select').chosen({
     width: '100%',
   });
-
-  $('form').on('submit', function() {
-    debugger;
-    e.preventDefault();
-    console.log($(this).serialize())
-    return false;
-  })
 }
