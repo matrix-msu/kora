21fb0a62865b17eb1dd7d5f9d5f1904a5fa2f008
Bogdan Pozderca <bogdanpozderca@gmail.com>
2017-09-17 04:23:09 -0400

    Minor Cleanup

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -29,17 +29,7 @@
 @stop
 
 @section('javascripts')
-  {!! Minify::javascript([
-    '/assets/javascripts/vendor/jquery/jquery.js',
-    '/assets/javascripts/vendor/jquery/jquery-ui.js',
-    '/assets/javascripts/vendor/chosen.js',
-    '/assets/javascripts/general/modal.js',
-    '/assets/javascripts/projects/create.js',
-    '/assets/javascripts/projects/index.js',
-    '/assets/javascripts/projects/show.js',
-    '/assets/javascripts/navigation/navigation.js',
-    '/assets/javascripts/general/global.js'
-  ])->withFullUrl() !!}
+  @include('partials.projects.javascripts')
 
   <script type="text/javascript">
     Kora.Projects.Create();
