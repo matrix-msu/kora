62c1191e86600e1f85488918b8d193af8628f088
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2017-09-20 15:01:43 -0400

    Project request email now has first last name

--- resources/views/emails/request/access.blade.php
+++ resources/views/emails/request/access.blade.php
@@ -1,5 +1,5 @@
 Hello Kora3 Project Administrator!
 <br><br>
-The user, {{\Auth::user()->username}} ({{\Auth::user()->name}}), is requesting access to the following project: {{$project->name}}
+The user, {{\Auth::user()->username}} ({{\Auth::user()->first_name}}, {{\Auth::user()->last_name}}), is requesting access to the following project: {{$project->name}}
 <br><br>
 Visit the Manage Groups page <a href="{{action('ProjectGroupController@index', ['pid'=>$project->pid])}}">here</a> to grant access
