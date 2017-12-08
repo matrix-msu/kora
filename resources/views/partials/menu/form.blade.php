58ad70af5b6d1142f9f72a2491446bfc20f32b93
Bogdan Pozderca <bogdanpozderca@gmail.com>
2017-12-07 22:07:47 -0500

    Navigation items

--- resources/views/partials/menu/form.blade.php
+++ resources/views/partials/menu/form.blade.php
@@ -1,53 +1,101 @@
-<li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ \App\Http\Controllers\FormController::getForm($fid)->name }}<b class="caret"></b></a>
-    <ul class="dropdown-menu">
-        <li><a href="{{ url('/projects/'.$pid).'/forms/'.$fid}}">{{trans('partials_menu_form.layout')}}</a></li>
-        <?php $allowed_forms = \Auth::user()->allowedForms($pid) ?>
-        @if(sizeof($allowed_forms) > 1)
-            <li class="dropdown-submenu" id="form-submenu"> <a href="#" data-toggle="dropdown">{{trans('partials_menu_form.jump')}}</a>
-                <ul class="dropdown-menu scrollable-submenu">
-                    @foreach($allowed_forms as $form)
-                        @if($form->fid != $fid)
-                            <li><a href="{{ url('/projects/'.$pid).'/forms/'.$form->fid }}">{{ $form->name }}</a></li>
-                        @endif
-                    @endforeach
-                </ul>
-            </li>
-        @endif
+<li class="navigation-item">
+  <a href="#" class="menu-toggle navigation-toggle-js">
+    <i class="icon icon-minus mr-sm"></i>
+    <span>{{ \App\Http\Controllers\FormController::getForm($fid)->name }}</span>
+    <i class="icon icon-chevron"></i>
+  </a>
 
-        @if(isset($passed_field))
-            <?php
-            $fieldsInForm = \App\Field::where('fid', '=', $fid)->get()->all();
-            $cnt = sizeof($fieldsInForm);
-            ?>
-            @if($cnt > 1)
-                <li class="dropdown-submenu" id="field-submenu"> <a href="#" data-toggle="dropdown">{{trans('partials_menu_options.jump')}}</a>
-                    <ul class="dropdown-menu scrollable-submenu">
-                        @foreach($fieldsInForm as $field)
-                            @if($field->flid != $passed_field->flid)
-                                <li><a href="{{ url('/projects/'.$pid).'/forms/'.$field->fid .'/fields/'.$field->flid.'/options'}}">{{ $field->name }}</a></li>
-                            @endif
-                        @endforeach
-                    </ul>
-                </li>
-            @endif
-        @endif
+  <ul class="navigation-sub-menu navigation-sub-menu-js">
+      <li class="link link-head">
+        <a href="{{ url('/projects/'.$pid).'/forms/'.$fid}}">
+          <i class="icon icon-form"></i>
+          <span>Form Home</span>
+        </a>
+      </li>
 
-        <li class="divider"></li>
-        <li><a href="{{ url('/projects/'.$pid).'/forms/'.$fid.'/records'}}">{{trans('partials_menu_form.records')}}</a></li>
-        @if(\Auth::user()->canIngestRecords(\App\Http\Controllers\FormController::getForm($fid)))
-            <li><a href="{{ action('RecordController@create',['pid' => $pid, 'fid' => $fid]) }}">{{trans('partials_menu_form.newrec')}}</a></li>
-            <li><a href="{{ action('RecordController@importRecordsView',['pid' => $pid, 'fid' => $fid]) }}">{{trans('partials_menu_form.import')}}</a></li>
+      <li class="spacer full"></li>
+
+      <li class="link">
+        <a href="{{action('FieldController@create', ['pid'=>$pid, 'fid' => $fid])}}">Create New Field</a>
+      </li>
+
+      @if(isset($passed_field))
+        <?php
+          $fieldsInForm = \App\Field::where('fid', '=', $fid)->get()->all();
+          $cnt = sizeof($fieldsInForm);
+        ?>
+        @if($cnt > 0)
+          <li class="link" id="form-submenu">
+            <a href='#' class="navigation-sub-menu-toggle-js" data-toggle="dropdown">
+              <span>Jump to Field</span>
+              <img class="icon" src="http://localhost:8888/Kora3/public/assets/images/menu_plus.svg">
+            </a>
+            <ul class="dropdown-menu scrollable-submenu">
+                @foreach($fieldsInForm as $field)
+                  @if($field->flid != $passed_field->flid)
+                    <li class="link">
+                      <a href="{{ url('/projects/'.$pid).'/forms/'.$field->fid .'/fields/'.$field->flid.'/options'}}">{{ $field->name }}</a>
+                    </li>
+                  @endif
+                @endforeach
+            </ul>
+          </li>
         @endif
-        <li><a href="{{ action('RecordController@showMassAssignmentView',['pid' => $pid, 'fid' => $fid]) }}">{{trans('partials_menu_form.massassign')}}</a></li>
-        <li class="divider"></li>
-        <li><a href="{{url('/projects/'.$pid).'/forms/'.$fid.'/metadata/setup'}}">{{trans('partials_menu_form.lod')}}</a></li>
-        @if (\Auth::user()->admin || \Auth::user()->isFormAdmin(\App\Http\Controllers\FormController::getForm($fid)))
-            <li class="divider"></li>
-            <li><a href="{{action('FormGroupController@index', ['pid'=>$pid, 'fid'=>$fid])}}">{{trans('partials_menu_form.groups')}}</a></li>
-            <li><a href="{{action('AssociationController@index', ['fid'=>$fid, 'pid'=>$pid])}}">{{trans('partials_menu_form.assoc')}}</a></li>
-            <li><a href="{{action('RevisionController@index', ['pid'=>$pid, 'fid'=>$fid])}}">{{trans('partials_menu_form.revisions')}}</a></li>
-            <li><a href="{{action('RecordPresetController@index', ['pid'=>$pid, 'fid'=>$fid])}}">{{trans('partials_menu_form.presets')}}</a></li>
-        @endif
-    </ul>
+      @endif
+
+      <li class="link">
+        <a href="{{ url('/projects/'.$pid).'/forms/'.$fid.'/records'}}">View Form Records</a>
+      </li>
+
+      @if(\Auth::user()->canIngestRecords(\App\Http\Controllers\FormController::getForm($fid)))
+        <li class="link">
+          <a href="{{ action('RecordController@create',['pid' => $pid, 'fid' => $fid]) }}">Create New Record</a>
+        </li>
+      @endif
+
+      @if(\Auth::user()->canIngestRecords(\App\Http\Controllers\FormController::getForm($fid)))
+        <li class="spacer full"></li>
+        <li class="link">
+          <a href="{{ action('RecordController@importRecordsView',['pid' => $pid, 'fid' => $fid]) }}">Import Records</a>
+        </li>
+      @endif
+
+      <li class="link">
+        <a href="{{ action('RecordController@showMassAssignmentView',['pid' => $pid, 'fid' => $fid]) }}">Batch Assign Field Values</a>
+      </li>
+
+      @if (\Auth::user()->admin || \Auth::user()->isFormAdmin(\App\Http\Controllers\FormController::getForm($fid)))
+        <li class="link">
+          <a href="{{action('RevisionController@index', ['pid'=>$pid, 'fid'=>$fid])}}">Manage Record Revisions</a>
+        </li>
+
+        <li class="link">
+          <a href="{{action('RecordPresetController@index', ['pid'=>$pid, 'fid'=>$fid])}}">Manage Record Presets</a>
+        </li>
+
+        <li class="link">
+          <a href="{{ action('ExportController@exportRecords',['fid'=>$fid, 'pid' => $pid]) }}">Export All Records</a>
+        </li>
+
+        <li class="link">
+          <a href="{{ action('FormController@edit', ['pid'=>$pid, 'fid'=>$fid]) }}">Edit Form Information</a>
+        </li>
+
+        <li class="link">
+          <a href="{{ action('FormGroupController@index', ['pid'=>$pid, 'fid'=>$fid]) }}">Form Permissions</a>
+        </li>
+
+        <li class="link">
+          <a href="{{action('AssociationController@index', ['fid'=>$fid, 'pid'=>$pid])}}">Association Permissions</a>
+        </li>
+
+        <li class="link">
+          <a href="{{ action('ExportController@exportForm',['fid'=>$fid, 'pid' => $pid]) }}">Export Form</a>
+        </li>
+      @endif
+
+      <li class="link">
+        <a href="{{url('/projects/'.$pid).'/forms/'.$fid.'/metadata/setup'}}">Link Open Data</a>
+      </li>
+  </ul>
 </li>
--- resources/views/partials/menu/project.blade.php
+++ resources/views/partials/menu/project.blade.php
@@ -1,71 +1,71 @@
 <li class="navigation-item">
-    <a href="#" class="menu-toggle navigation-toggle-js">
-      <i class="icon icon-minus mr-sm"></i>
-      <span>{{ \App\Http\Controllers\ProjectController::getProject($pid)->name }}</span>
-      <i class="icon icon-chevron"></i>
-    </a>
-    <ul class="navigation-sub-menu navigation-sub-menu-js">
-      <li class="link link-head">
-        <a href="{{ url('/projects/'.$pid) }}">
-          <i class="icon icon-project"></i>
-          <span>Project Home</span>
-        </a>
+  <a href="#" class="menu-toggle navigation-toggle-js">
+    <i class="icon icon-minus mr-sm"></i>
+    <span>{{ \App\Http\Controllers\ProjectController::getProject($pid)->name }}</span>
+    <i class="icon icon-chevron"></i>
+  </a>
+  <ul class="navigation-sub-menu navigation-sub-menu-js">
+    <li class="link link-head">
+      <a href="{{ url('/projects/'.$pid) }}">
+        <i class="icon icon-project"></i>
+        <span>Project Home</span>
+      </a>
+    </li>
+
+    @if (\Auth::user()->admin ||  \Auth::user()->isProjectAdmin(\App\Http\Controllers\ProjectController::getProject($pid)))
+      <li class="spacer full"></li>
+
+      <li class="link">
+        <a href="{{action('FormController@create', ['pid'=>$pid])}}">Create New Form</a>
       </li>
 
-      @if (\Auth::user()->admin ||  \Auth::user()->isProjectAdmin(\App\Http\Controllers\ProjectController::getProject($pid)))
-        <li class="spacer full"></li>
+      <li class="link">
+        <a href="{{action('FormController@importFormView', ['pid'=>$pid])}}">Import Form Setup</a>
+      </li>
 
-        <li class="link">
-          <a href="{{action('FormController@create', ['pid'=>$pid])}}">Create New Form</a>
-        </li>
+      <?php $allowed_forms = \Auth::user()->allowedForms($pid) ?>
+      @if(sizeof($allowed_forms) > 0 )
+        <li class="link" id="project-submenu">
+          <a href='#' class="navigation-sub-menu-toggle-js" data-toggle="dropdown">
+            <span>Jump to Form</span>
+            <img class="icon" src="http://localhost:8888/Kora3/public/assets/images/menu_plus.svg">
+          </a>
 
-        <li class="link">
-          <a href="{{action('FormController@importFormView', ['pid'=>$pid])}}">Import Form Setup</a>
-        </li>
-
-        <?php $allowed_forms = \Auth::user()->allowedForms($pid) ?>
-        @if(sizeof($allowed_forms) > 0 )
-          <li class="link" id="project-submenu">
-            <a href='#' class="navigation-sub-menu-toggle-js" data-toggle="dropdown">
-              <span>Jump to Form</span>
-              <img class="icon" src="http://localhost:8888/Kora3/public/assets/images/menu_plus.svg">
-            </a>
-
-            <ul class="navigation-deep-menu navigation-deep-menu-js">
-              @foreach($allowed_forms as $form)
-                <li class="link">
-                  <a href="{{ action('FormController@show', ['pid'=>$pid, 'fid' => $form->fid]) }}">{{ $form->name }}</a>
-                </li>
-              @endforeach
-            </ul>
-          </li>
-        @endif
-
-        <li class="link">
-          <a href="#">Search Project Records</a>
-        </li>
-
-        <li class="spacer"></li>
-
-        <li class="link">
-          <a href="{{ action('ProjectController@edit', ['pid'=>$pid]) }}">Edit Project Information</a>
-        </li>
-
-        <li class="link">
-          <a href="{{ action('ProjectGroupController@index', ['pid'=>$pid]) }}">Project Permissions</a>
-        </li>
-
-        <li class="link">
-          <a href="{{ action('OptionPresetController@index',['pid' => $pid]) }}">Field Value Presets</a>
-        </li>
-
-        <li class="link">
-          <a href="{{ action('FormController@importFormViewK2',['pid' => $pid]) }}">Kora 2 Scheme Importer</a>
-        </li>
-
-        <li class="link">
-          <a href="{{ action('ExportController@exportProject',['pid' => $pid]) }}">Export Project</a>
+          <ul class="navigation-deep-menu navigation-deep-menu-js">
+            @foreach($allowed_forms as $form)
+              <li class="link">
+                <a href="{{ action('FormController@show', ['pid'=>$pid, 'fid' => $form->fid]) }}">{{ $form->name }}</a>
+              </li>
+            @endforeach
+          </ul>
         </li>
       @endif
-    </ul>
+
+      <li class="link">
+        <a href="#">Search Project Records</a>
+      </li>
+
+      <li class="spacer"></li>
+
+      <li class="link">
+        <a href="{{ action('ProjectController@edit', ['pid'=>$pid]) }}">Edit Project Information</a>
+      </li>
+
+      <li class="link">
+        <a href="{{ action('ProjectGroupController@index', ['pid'=>$pid]) }}">Project Permissions</a>
+      </li>
+
+      <li class="link">
+        <a href="{{ action('OptionPresetController@index',['pid' => $pid]) }}">Field Value Presets</a>
+      </li>
+
+      <li class="link">
+        <a href="{{ action('FormController@importFormViewK2',['pid' => $pid]) }}">Kora 2 Scheme Importer</a>
+      </li>
+
+      <li class="link">
+        <a href="{{ action('ExportController@exportProject',['pid' => $pid]) }}">Export Project</a>
+      </li>
+    @endif
+  </ul>
 </li>
