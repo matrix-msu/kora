46be819dc1ff02194ef87675b72e96d47ba5eae4
Nicholas Mikelsavage <nicholas.mikelsavage@peach.matrix.msu.edu>
2018-01-25 09:11:57 -0500

    simplified footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,4 @@
-<footer>
-  <div class="footer-content mx-auto py-100-xl">
-    <p class="footer-item mx-xl my-0"><img src="{{ config('app.url') }}logos/koraiii-logo-white.svg" alt="KORA"></p>
-    <p class="footer-item mx-xl my-0"><img src="{{ config('app.url') }}logos/koraiii-logo-blue.svg" alt="Matrix"></p>
-  </div>
+<footer class="py-100-xl">
+  <p class="footer-item mx-xl my-0"><img src="{{ config('app.url') }}logos/koraiii-logo-white.svg" alt="KORA"></p>
+  <p class="footer-item mx-xl my-0"><img src="{{ config('app.url') }}logos/koraiii-logo-blue.svg" alt="Matrix"></p>
 </footer>
