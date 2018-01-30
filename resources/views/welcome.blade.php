d4428a0c2742a92af21da5ae0114b5ebcf1dd902
Nicholas Mikelsavage <nicholas.mikelsavage@peach.matrix.msu.edu>
2018-01-30 12:55:03 -0500

    login modal header spacing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,65 +7,64 @@
       <img src="{{ config('app.url') }}logos/koraiii-logo-blue.svg">
     </div>
 
-      <div>
-        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-          <div class="form-group mt-xxxl">
-            {!! Form::label('email', 'Your Username or Email') !!}
-            {!! Form::text('email', null, ['class' => 'text-input', 'placeholder' => 'Enter your username or email here', 'value' => old('email'), 'autofocus']) !!}
-          </div>
-
-          <div class="form-group mt-xl">
-            {!! Form::label('password', 'Your Password') !!}
-            {!! Form::password('password', ['class' => 'text-input', 'placeholder' => 'Enter your password here']) !!}
-          </div>
-
-          <div class="form-group mt-xxxl memory">
-            <div class="check-box-half">
-              <input type="checkbox"
-                     value="1"
-                     class="check-box-input"
-                     id="remember"
-                     name="remember" />
-              <span class="check"></span>
-              <span class="placeholder">Remember Me</span>
-            </div>
-
-            <p class="forgot my-0"><a class="text password-js underline-middle-hover" href="#">Forgot Password?</a></p>
-          </div>
-
-          <div class="form-group mt-xxxl">
-            <button type="submit" class="btn btn-primary">Login</button>
-          </div>
-
-          <p class="mt-xxxl mb-0"><a class="text  underline-middle-hover" href="{{ url('/register') }}">Need to Sign Up?</a></p>
-        </form>
+    @if (count($errors) > 0)
+      <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
       </div>
+    @endif
 
+    <div>
+      <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+        <div class="form-group mt-xxxl">
+          {!! Form::label('email', 'Your Username or Email') !!}
+          {!! Form::text('email', null, ['class' => 'text-input', 'placeholder' => 'Enter your username or email here', 'value' => old('email'), 'autofocus']) !!}
+        </div>
+
+        <div class="form-group mt-xl">
+          {!! Form::label('password', 'Your Password') !!}
+          {!! Form::password('password', ['class' => 'text-input', 'placeholder' => 'Enter your password here']) !!}
+        </div>
+
+        <div class="form-group mt-xxxl memory">
+          <div class="check-box-half">
+            <input type="checkbox"
+                   value="1"
+                   class="check-box-input"
+                   id="remember"
+                   name="remember" />
+            <span class="check"></span>
+            <span class="placeholder">Remember Me</span>
+          </div>
+
+          <p class="forgot my-0"><a class="text password-js underline-middle-hover" href="#">Forgot Password?</a></p>
+        </div>
+
+        <div class="form-group mt-xxxl">
+          <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+
+        <p class="mt-xxxl mb-0"><a class="text  underline-middle-hover" href="{{ url('/register') }}">Need to Sign Up?</a></p>
+      </form>
     </div>
+  </div>
 </div>
 
 <div class="modal modal-js modal-mask">
   <div class="content">
     <div class="header">
-      <span class="title">Forgot Your Password?</span>
+      <span class="title pb-0">Forgot Your Password?</span>
       <a href="#" class="modal-toggle modal-toggle-js">
         <i class="icon icon-cancel"></i>
       </a>
     </div>
     <div class="body">
-      @if (count($errors) > 0)
-        <div class="alert alert-danger">
-          <strong>{{trans('auth_password.whoops')}}!</strong> {{trans('auth_password.problems')}}.<br><br>
-          <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-          </ul>
-        </div>
-      @endif
-
       <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
