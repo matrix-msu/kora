e39e76e08c0eea132120650a24d1cdced3986f26
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2018-03-16 08:55:52 -0400

    Made basic 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,39 +1,9 @@
-@extends('app')
+@extends('app', ['page_title' => 'No Page Found', 'page_class' => 'error-404'])
 
-@section('content')
-    <style>
-        .body_error {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            color: #B0BEC5;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container_error {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content_error {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title_error {
-            font-size: 72px;
-            margin-bottom: 40px;
-        }
-    </style>
-    <div class="body_error">
-        <div class="container_error">
-            <div class="content_error">
-                <div class="title_error">{{trans('errors_404.droids')}} 4(04).</div>
-            </div>
+@section('body')
+    <div class="content">
+        <div class="form-container center">
+            <div class="404-error">These are not the droids you are looking 4(04).</div>
         </div>
     </div>
 @endsection
