b80ccc32b58602b854f8cbbb7b006578c09c56a3
Joshua Miles <milesjos@msu.edu>
2018-04-03 15:54:09 -0400

    Make pagination numbers two wide

--- resources/views/revisions/index.blade.php
+++ resources/views/revisions/index.blade.php
@@ -70,8 +70,11 @@
             @if (!$revisions->onFirstPage())
                 <a href="{{$revisions->url(1)}}" class="page-link">1</a>
                 @if ($revisions->currentPage() > 2)
-                    @if ($revisions->currentPage() > 3)
+                    @if ($revisions->currentPage() > 4)
                         <span class="page-link">...</span>
+                    @endif
+                    @if ($revisions->currentPage() > 3)
+                        <a href="{{$revisions->url($revisions->currentPage()-2)}}" class="page-link">{{$revisions->currentPage() - 2}}</a>
                     @endif
                     <a href="{{$revisions->previousPageUrl()}}" class="page-link">{{$revisions->currentPage() - 1}}</a>
                 @endif
@@ -81,6 +84,9 @@
                 @if ($revisions->lastPage() - 1 > $revisions->currentPage())
                     <a href="{{$revisions->url($revisions->currentPage()+1)}}" class="page-link">{{$revisions->currentPage() + 1}}</a>
                     @if ($revisions->lastPage() - 2 > $revisions->currentPage())
+                        <a href="{{$revisions->url($revisions->currentPage()+2)}}" class="page-link">{{$revisions->currentPage() + 2}}</a>
+                    @endif
+                    @if ($revisions->lastPage() - 3 > $revisions->currentPage())
                         <span class="page-link">...</span>
                     @endif
                 @endif
