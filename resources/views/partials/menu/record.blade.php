b76c0707082b12f65ddc1c536af8b6953ab2b692
Joshua Miles <milesjos@msu.edu>
2018-04-12 14:21:14 -0400

    Make the record menu dropdown have the correct links

--- resources/views/partials/menu/record.blade.php
+++ resources/views/partials/menu/record.blade.php
@@ -1,6 +1,34 @@
 <li class="navigation-item">
-    <a href="{{action("RecordController@show", ['pid'=>$record->pid,'fid'=>$record->fid,'rid'=>$record->rid])}}" class="menu-toggle navigation-toggle-js">
+    <a href="#" class="menu-toggle navigation-toggle-js">
         <i class="icon icon-minus mr-sm"></i>
         <span>{{ $record->kid }}</span>
+        <i class="icon icon-chevron"></i>
     </a>
+
+    <ul class="navigation-sub-menu navigation-sub-menu-js">
+        <li class="link link-head">
+            <a href="{{action("RecordController@show", ['pid'=>$record->pid,'fid'=>$record->fid,'rid'=>$record->rid])}}">
+                <i class="icon icon-record"></i>
+                <span>{{ $record->kid }}</span>
+            </a>
+        </li>
+
+        <li class="spacer full"></li>
+
+        <li class="link first">
+            <a href="{{action("RecordController@edit", ['pid'=>$record->pid, 'fid'=>$record->fid, 'rid'=>$record->rid])}}">Edit Record</a>
+        </li>
+
+        <li class="link">
+            <a href="{{action('RecordController@cloneRecord', ['pid'=>$record->pid, 'fid'=>$record->fid, 'rid'=>$record->rid])}}">Duplicate Record</a>
+        </li>
+
+        <li class="link">
+            <a href="{{action("RevisionController@show", ['pid'=>$record->pid, 'fid'=>$record->fid, 'rid'=>$record->rid])}}">View Revisions ({{\App\Http\Controllers\RevisionController::getRevisionCount($record->rid)}})</a>
+        </li>
+
+        <li class="link">
+            <a href="#">Designate as Preset</a>
+        </li>
+    </ul>
 </li>
