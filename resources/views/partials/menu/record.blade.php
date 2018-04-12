6bf4cdabe66f7a7acf7cc721de51ccd8857a5127
Joshua Miles <milesjos@msu.edu>
2018-04-12 15:53:35 -0400

    Fix record dropdown partial/pages that use it

--- resources/views/partials/menu/record.blade.php
+++ resources/views/partials/menu/record.blade.php
@@ -1,30 +1,30 @@
 <li class="navigation-item">
     <a href="#" class="menu-toggle navigation-toggle-js">
         <i class="icon icon-minus mr-sm"></i>
-        <span>{{ $record->kid }}</span>
+        <span>{{ $pid . '-' . $fid . '-' . $rid }}</span>
         <i class="icon icon-chevron"></i>
     </a>
 
     <ul class="navigation-sub-menu navigation-sub-menu-js">
         <li class="link link-head">
-            <a href="{{action("RecordController@show", ['pid'=>$record->pid,'fid'=>$record->fid,'rid'=>$record->rid])}}">
+            <a href="{{action("RecordController@show", ['pid'=>$pid,'fid'=>$fid,'rid'=>$rid])}}">
                 <i class="icon icon-record"></i>
-                <span>{{ $record->kid }}</span>
+                <span>{{ $pid . '-' . $fid . '-' . $rid }}</span>
             </a>
         </li>
 
         <li class="spacer full"></li>
 
         <li class="link first">
-            <a href="{{action("RecordController@edit", ['pid'=>$record->pid, 'fid'=>$record->fid, 'rid'=>$record->rid])}}">Edit Record</a>
+            <a href="{{action("RecordController@edit", ['pid'=>$pid, 'fid'=>$fid, 'rid'=>$rid])}}">Edit Record</a>
         </li>
 
         <li class="link">
-            <a href="{{action('RecordController@cloneRecord', ['pid'=>$record->pid, 'fid'=>$record->fid, 'rid'=>$record->rid])}}">Duplicate Record</a>
+            <a href="{{action('RecordController@cloneRecord', ['pid'=>$pid, 'fid'=>$fid, 'rid'=>$rid])}}">Duplicate Record</a>
         </li>
 
         <li class="link">
-            <a href="{{action("RevisionController@show", ['pid'=>$record->pid, 'fid'=>$record->fid, 'rid'=>$record->rid])}}">View Revisions ({{\App\Http\Controllers\RevisionController::getRevisionCount($record->rid)}})</a>
+            <a href="{{action("RevisionController@show", ['pid'=>$pid, 'fid'=>$fid, 'rid'=>$rid])}}">View Revisions ({{\App\Http\Controllers\RevisionController::getRevisionCount($rid)}})</a>
         </li>
 
         <li class="link">
