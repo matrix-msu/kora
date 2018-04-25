4bc17399844fa0053cd14ae0b2e487badfbd6b96
Joshua Miles <milesjos@msu.edu>
2018-04-25 14:55:56 -0400

    Fix pagination on create page to work consistently

--- resources/views/partials/records/pagination-form.blade.php
+++ resources/views/partials/records/pagination-form.blade.php
@@ -1,39 +1,17 @@
 <section class="pagination center">
-    <div class="previous page {{$form->onFirstPage() ? 'disabled' : ''}}">
-        <a href="{{$form->previousPageUrl()}}">
+    <div class="previous page disabled">
+        <a href="#">
             <i class="icon icon-chevron left"></i>
             <span class="name underline-middle-hover">Previous Page</span>
         </a>
     </div>
     <div class="pages">
-        @if (!$form->onFirstPage())
-            <a href="{{$form->url(1)}}" class="page-link">1</a>
-            @if ($form->currentPage() > 2)
-                @if ($form->currentPage() > 4)
-                    <span class="page-link">...</span>
-                @endif
-                @if ($form->currentPage() > 3)
-                    <a href="{{$form->url($form->currentPage()-2)}}" class="page-link">{{$form->currentPage() - 2}}</a>
-                @endif
-                <a href="{{$form->previousPageUrl()}}" class="page-link">{{$form->currentPage() - 1}}</a>
-            @endif
-        @endif
-        <a href="" class="page-link active">{{$form->currentPage()}}</a>
-        @if ($form->lastPage() !== $form->currentPage())
-            @if ($form->lastPage() - 1 > $form->currentPage())
-                <a href="{{$form->url($form->currentPage()+1)}}" class="page-link">{{$form->currentPage() + 1}}</a>
-                @if ($form->lastPage() - 2 > $form->currentPage())
-                    <a href="{{$form->url($form->currentPage()+2)}}" class="page-link">{{$form->currentPage() + 2}}</a>
-                @endif
-                @if ($form->lastPage() - 3 > $form->currentPage())
-                    <span class="page-link">...</span>
-                @endif
-            @endif
-            <a href="{{$form->url($form->lastPage())}}" class="page-link">{{$form->lastPage()}}</a>
-        @endif
+        @foreach ($layout as $pageNumber => $page)
+            <a href="#" class="page-link {{$pageNumber === 0 ? 'active' : ''}}">{{$pageNumber + 1}}</a>
+        @endforeach
     </div>
-    <div class="next page {{$form->hasMorePages() ? '' : 'disabled'}}">
-        <a href="{{$form->nextPageUrl()}}">
+    <div class="next page {{count($layout) > 1 ? '' : 'disabled'}}">
+        <a href="#">
             <i class="icon icon-chevron right"></i>
             <span class="name underline-middle-hover">Next Page</span>
         </a>
