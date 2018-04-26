31fd5d4e5345b052f4ed151d5df2591c1edcbfe1
Joshua Miles <milesjos@msu.edu>
2018-04-26 14:41:04 -0400

    Add basic front end validation to the create projects page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::resource('projects', 'ProjectController');
     Route::post('projects/request', 'ProjectController@request');
     Route::post('projects/{pid}/archive', 'ProjectController@setArchiveProject');
+    Route::post('projects/validate', 'ProjectController@validateProjectFields');
 
 //project group routes
     Route::get('/projects/{pid}/manage/projectgroups/{active?}', 'ProjectGroupController@index');
--- public/assets/javascripts/projects/create.js
+++ public/assets/javascripts/projects/create.js
@@ -6,4 +6,38 @@ Kora.Projects.Create = function() {
   $('.multi-select').chosen({
     width: '100%',
   });
+
+  function initializeValidation() {
+    $('form input.btn').on('click', function(e) {
+      var $this = $(this);
+
+      e.preventDefault();
+
+      values = {};
+      $.each($('form').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+      });
+
+      $.ajax({
+        url: validationUrl,
+        method: 'POST',
+        data: values,
+        success: function(data) {
+          $('form').submit();
+        },
+        error: function(err) {
+          $('.error-message').text('');
+          $('.text-input, .text-area').removeClass('error');
+
+          $.each(err.responseJSON, function(fieldName, errors) {
+            var $field = $('#'+fieldName);
+            $field.addClass('error');
+            $field.siblings('.error-message').text(errors[0]);
+          });
+        }
+      });
+    });
+  }
+
+  initializeValidation();
 }
