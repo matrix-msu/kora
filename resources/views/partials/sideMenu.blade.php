9bc1854c534c36f82b22de799623e05a49708434
Bogdan Pozderca <bogdan.pozderca@gmail.com>
2018-05-02 17:24:50 -0400

    Added feedback icon and link on sidebar

--- resources/views/partials/sideMenu.blade.php
+++ resources/views/partials/sideMenu.blade.php
@@ -11,13 +11,21 @@
     </div>
     <div class="footer-elements">
       <div class="drawer-element">
+        <a target="_blank" href="https://github.com/matrix-msu/Kora3/issues" class="drawer-toggle">
+          <i class="icon icon-feedback"></i>
+          <span>Submit Feedback</span>
+          <i class="icon icon-external-link"></i>
+        </a>
+      </div>
+
+      <div class="drawer-element">
         <a target="_blank" href="https://github.com/matrix-msu/Kora3" class="drawer-toggle">
           <i class="icon icon-help"></i>
           <span>Help & Documentation</span>
           <i class="icon icon-external-link"></i>
         </a>
       </div>
-
+      
       @include('partials.sideMenu.management')
       @include('partials.sideMenu.plugins')
     <div>
