da2ab1a020a250855ce6c67e16172d236e851b68
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2018-05-04 15:50:24 -0400

    Styling changes for export records modal

--- resources/views/partials/records/modals/exportRecordsModal.blade.php
+++ resources/views/partials/records/modals/exportRecordsModal.blade.php
@@ -10,14 +10,14 @@
             <div class="form-group">
                 Export all form records in the formats of JSON or XML. You may also export all record files as a zip.
             </div>
-            <div class="form-group half mt-m">
-                <a href="{{action('ExportController@exportRecords', ['pid' => $pid, 'fid' => $fid, 'type' => 'JSON'])}}" class="btn secondary pr-xs">Export JSON</a>
+            <div class="form-group mt-m">
+                <a href="{{ action('ExportController@exportRecordFiles',['pid' => $form->pid, 'fid' => $form->fid]) }}" class="btn secondary">Export Record Files</a>
             </div>
-            <div class="form-group half mt-m">
-                <a href="{{action('ExportController@exportRecords', ['pid' => $pid, 'fid' => $fid, 'type' => 'XML'])}}" class="btn secondary pl-xs">Export XML</a>
+            <div class="form-group mt-m">
+                <a href="{{action('ExportController@exportRecords', ['pid' => $pid, 'fid' => $fid, 'type' => 'JSON'])}}" class="btn">Export JSON</a>
             </div>
             <div class="form-group">
-                <a href="{{ action('ExportController@exportRecordFiles',['pid' => $form->pid, 'fid' => $form->fid]) }}" class="btn">Export Record Files</a>
+                <a href="{{action('ExportController@exportRecords', ['pid' => $pid, 'fid' => $fid, 'type' => 'XML'])}}" class="btn">Export XML</a>
             </div>
         </div>
     </div>
