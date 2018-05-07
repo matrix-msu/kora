076179ff43209ee6743eeb22edf83bd4c708a626
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2018-05-07 14:33:33 -0400

    fixed mobile for close open record cards

--- resources/views/globalSearch/results.blade.php
+++ resources/views/globalSearch/results.blade.php
@@ -83,8 +83,8 @@
                         </select>
                     </div>
                     <div class="show-options show-options-js">
-                        <a href="#" class="expand-fields-js" title="Expand all fields"><i class="icon icon-expand icon-expand-js"></i></a>
-                        <a href="#" class="collapse-fields-js" title="Collapse all fields"><i class="icon icon-condense icon-condense-js"></i></a>
+                        <span><a href="#" class="expand-fields-js" title="Expand all fields"><i class="icon icon-expand icon-expand-js"></i></a></span>
+                        <span><a href="#" class="collapse-fields-js" title="Collapse all fields"><i class="icon icon-condense icon-condense-js"></i></a></span>
                     </div>
                 </section>
 
