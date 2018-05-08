4622c51d2ac39da1c28fa24db980a54eca799d0e
Anthony Donofrio <anthony.donofrio@peach.matrix.msu.edu>
2018-05-08 12:39:45 -0400

    Added user validation to the login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,29 +5,20 @@
   <div class="form-container center">
     <img class="logo" src="{{ config('app.url') }}assets/logos/koraiii-logo-blue.svg">
 
-    @if (count($errors) > 0)
-      <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-          @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-          @endforeach
-        </ul>
-      </div>
-    @endif
-
     <div>
       <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
         <div class="form-group mt-xxxl">
           {!! Form::label('email', 'Your Username or Email') !!}
-          {!! Form::text('email', null, ['class' => 'text-input', 'placeholder' => 'Enter your username or email here', 'value' => old('email'), 'autofocus']) !!}
+            <span class="error-message">{{array_key_exists("email", $errors->messages()) ? $errors->messages()["email"][0] : ''}}</span>
+          {!! Form::text('email', null, ['class' => 'text-input'. (array_key_exists("email", $errors->messages()) ? ' error' : ''), 'placeholder' => 'Enter your username or email here', 'value' => old('email'), 'autofocus']) !!}
         </div>
 
         <div class="form-group mt-xl">
           {!! Form::label('password', 'Your Password') !!}
-          {!! Form::password('password', ['class' => 'text-input', 'placeholder' => 'Enter your password here']) !!}
+            <span class="error-message">{{array_key_exists("password", $errors->messages()) ? $errors->messages()["password"][0] : ''}}</span>
+          {!! Form::password('password', ['class' => 'text-input'. (array_key_exists("password", $errors->messages()) ? ' error' : ''), 'placeholder' => 'Enter your password here']) !!}
         </div>
 
         <div class="form-group mt-xxxl memory">
@@ -63,16 +54,17 @@
       </a>
     </div>
     <div class="body">
-      <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+      <form class="form-horizontal pass-form-js" role="form" method="POST" action="{{ url('/password/email') }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
         <div class="form-group mt-xxl">
           <label for="email">Enter Your Email to Recover Password</label>
-          <input type="email" class="text-input" name="email" value="{{ old('email') }}" placeholder="Enter your email here">
+            <span class="error-message pass-error-js">{{array_key_exists("email", $errors->messages()) ? $errors->messages()["email"][0] : ''}}</span>
+          <input type="email" class="text-input pass-email-js {{(array_key_exists("email", $errors->messages()) ? ' error' : '')}}" name="email" value="{{ old('email') }}" placeholder="Enter your email here">
         </div>
 
         <div class="form-group mt-xxl">
-          <button type="submit" class="btn btn-primary">Send Password Reset Link</button>
+          <button type="submit" class="btn btn-primary pass-reset-js">Send Password Reset Link</button>
         </div>
       </form>
     </div>
@@ -91,5 +83,26 @@
 
       Kora.Modal.open();
     });
+
+    $('.pass-reset-js').click(function(e) {
+        e.preventDefault();
+
+        var email = $('.pass-email-js').val();
+
+        if(email=='') {
+            $('.pass-error-js').text('The email field is required.');
+            $('.pass-email-js').addClass('error');
+        } else if(!validateEmail(email)) {
+            $('.pass-error-js').text('The email must be a valid email address.');
+            $('.pass-email-js').addClass('error');
+        } else {
+            $('.pass-form-js').submit();
+        }
+    });
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
   </script>
 @stop
