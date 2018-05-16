5b4e1bf3865dd0353508bc86aa0ec93899c8b047
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-05-16 14:42:02 -0400

    fixes breadcrumb navigation styling issues

--- public/assets/javascripts/navigation/breadcrumbs.js
+++ public/assets/javascripts/navigation/breadcrumbs.js
@@ -1,38 +1,7 @@
-function collision($div1, $div2) {
-  var x1 = $div1.offset().left;
-  var y1 = $div1.offset().top;
-  var h1 = $div1.outerHeight(true);
-  var w1 = $div1.outerWidth(true);
-  var b1 = y1 + h1;
-  var r1 = x1 + w1;
-  var x2 = $div2.offset().left;
-  var y2 = $div2.offset().top;
-  var h2 = $div2.outerHeight(true);
-  var w2 = $div2.outerWidth(true);
-  var b2 = y2 + h2;
-  var r2 = x2 + w2;
-
-  if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-  return true;
-}
-
-var result
 window.setInterval(function() {
-  result = collision($('.navigation-left .navigation-item:last-child'), $('.navigation-search'));
-  console.log('' + result)
-  if (result === true) {
+  if (window.innerWidth <= 768) {
     $('.navigation-left').addClass('collapsed');
+  } else if (window.innerWidth > 768) {
+    $('.navigation-left').removeClass('collapsed');
   }
-  unsetBreadCrumbs ()
-}, 200);
-
-// can't use the above function to unset breadcrumbs because immediately on collapse the above function returns false and would unset and reset recursively
-function unsetBreadCrumbs () {
-  if (window.innerWidth >= 700 && $('.navigation-left').hasClass('collapsed')) {
-    $('.collapsed').removeClass('collapsed');
-  }
-}
-
-// this script is meant to detect when the left section of the nav (breadcrumbs) collide with the right side of the nav (nav-search)
-// needs to be done this way because the width of left-nav is variable
-// when collision = true, apply .collapsed to the <ul.nav-left>, and css should handle the rest
+})
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -37,69 +37,36 @@
   </ul>
 
   <ul class="navigation-right navigation-right-js">
-    @if(Auth::guest())
-        <li class="navigation-item pl-0">
-            <a href="#" class="text menu-toggle navigation-toggle-js underline-middle-hover">
-                <span>English</span>
-                <i class="icon icon-chevron"></i>
-            </a>
-            <ul class="navigation-sub-menu navigation-sub-menu-js language-select">
-                @foreach(getLangs()->keys() as $lang)
-                    <li><a onclick='setTempLang({{$lang}})' href='#'>{{\Illuminate\Support\Facades\Config::get('app.locales_supported')->get($lang)[1]}}</a> </li>
-                @endforeach
-            </ul>
+    <div class="navigation-right-wrap">
+      @if(Auth::guest())
+          <li class="navigation-item pl-0">
+              <a href="#" class="text menu-toggle navigation-toggle-js underline-middle-hover">
+                  <span>English</span>
+                  <i class="icon icon-chevron"></i>
+              </a>
+              <ul class="navigation-sub-menu navigation-sub-menu-js language-select">
+                  @foreach(getLangs()->keys() as $lang)
+                      <li><a onclick='setTempLang({{$lang}})' href='#'>{{\Illuminate\Support\Facades\Config::get('app.locales_supported')->get($lang)[1]}}</a> </li>
+                  @endforeach
+              </ul>
+          </li>
+      @elseif (!Auth::user()->active)
+        <li class="navigation-item">
+          <form id="logout_link" class="form-horizontal" role="form" method="POST" action="{{ url('/logout') }}">
+              <input type="hidden" name="_token" value="{{ csrf_token() }}">
+              <a class="logout underline-middle-hover">Logout</a>
+          </form>
         </li>
-    @elseif (!Auth::user()->active)
-      <li class="navigation-item">
-        <form id="logout_link" class="form-horizontal" role="form" method="POST" action="{{ url('/logout') }}">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <a class="logout underline-middle-hover">Logout</a>
-        </form>
-      </li>
-    @else
-      @include("partials.menu.globalSearch")
-      @include("partials.menu.userProfile")
-      @include("partials.menu.sideMenu")
-    @endif
+      @else
+        @include("partials.menu.globalSearch")
+        @include("partials.menu.userProfile")
+        @include("partials.menu.sideMenu")
+      @endif
+    </div>
   </ul>
-  <!--<script src="/assets/javascripts/navigation/breadcrumbs.js"></script>-->
-  <script type="text/javascript">
-    function collision($div1, $div2) {
-      var x1 = $div1.offset().left;
-      var y1 = $div1.offset().top;
-      var h1 = $div1.outerHeight(true);
-      var w1 = $div1.outerWidth(true);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = $div2.offset().left;
-      var y2 = $div2.offset().top;
-      var h2 = $div2.outerHeight(true);
-      var w2 = $div2.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
-
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-      return true;
-    }
-    
-    var result
-    window.setInterval(function() {
-      result = collision($('.navigation-left .navigation-item:last-child'), $('.navigation-search'));
-      console.log('' + result)
-      if (result === true) {
-        $('.navigation-left').addClass('collapsed');
-      }
-      unsetBreadCrumbs ()
-    }, 200);
-    
-    function unsetBreadCrumbs () {
-      if (window.innerWidth >= 700 && $('.navigation-left').hasClass('collapsed')) {
-        $('.collapsed').removeClass('collapsed');
-      }
-    }
-    
-  </script>
   
+  <script src="{{ config('app.url') }}/assets/javascripts/navigation/breadcrumbs.js"></script>
+    
   <script type="text/javascript">
     var globalQuickSearchUrl = '{{ action('ProjectSearchController@globalQuickSearch') }}';
     var globalSearchUrl = '{{action('ProjectSearchController@globalSearch')}}';
