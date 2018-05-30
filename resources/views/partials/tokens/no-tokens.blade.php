9c6e138f065c0d4d46edfbab93d6cdc9a6ce7b4c
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-05-30 11:42:54 -0400

    first commit of no-tokens.scss and no-tokens.blade

---
+++ resources/views/partials/tokens/no-tokens.blade.php
@@ -0,0 +1,9 @@
+<div class="no-tokens pt-xxxl">
+  <div class="top">
+    <div class="line"></div>
+    <i class="icon icon-form"></i>
+    <div class="line"></div>
+  </div>
+  <p>No tokens exist.</p>
+  <p><a type="button" class="create-token-js">Create new tokens</a> to get started</p>
+</div>
