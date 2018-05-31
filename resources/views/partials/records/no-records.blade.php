0f989138c609f233f3b92c1c96b9250f69a07020
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-05-31 09:10:52 -0400

    adding no-records.scss, records/no-records.blade, records/search-terms.blade

---
+++ resources/views/partials/records/no-records.blade.php
@@ -0,0 +1,10 @@
+<div class="no-records pt-xxxl">
+  <div class="top pb-xxxl">
+    <div class="line"></div>
+    <i class="icon icon-record"></i>
+    <div class="line"></div>
+  </div>
+  <div class="bottom">
+    <p>No Results Found!</p>
+  </div>
+</div>
---
+++ resources/views/partials/records/search-terms.blade.php
@@ -0,0 +1,8 @@
+<div class="searched-terms pt-xl">
+  <ul class="chosen-choices">
+    <li class="search-choice">
+      <span>search terms here</span>
+      <a class="search-choice-close" data-option-array-index="0"></a>
+    </li>
+  </ul>
+</div>
