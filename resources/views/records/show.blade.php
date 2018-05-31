e00063ea01c33c2a08b2f2000f2017ffadb8371d
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-05-31 09:19:33 -0400

    fixing universal back button

--- resources/views/records/show.blade.php
+++ resources/views/records/show.blade.php
@@ -23,7 +23,7 @@
 
 @section('header')
     <section class="head">
-        <a class="rotate" href="{{ URL::previous() }}"><i class="icon icon-chevron"></i></a>
+        <a class="back" href="{{ URL::previous() }}"><i class="icon icon-chevron"></i></a>
         <div class="inner-wrap center">
             <h1 class="title">
                 <i class="icon icon-record mr-sm"></i>
