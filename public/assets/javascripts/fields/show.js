2e7f218b2140a705af1d64e5e3af9ccf18a0cce8
RD <devolde2@msu.edu>
2018-06-15 11:31:04 -0400

    fixed validation callbacks being indexed incorrectly due to php version switch

--- public/assets/javascripts/fields/show.js
+++ public/assets/javascripts/fields/show.js
@@ -331,7 +331,7 @@ Kora.Fields.Show = function() {
                     $('.error-message').text('');
                     $('.text-input, .text-area').removeClass('error');
 
-                    $.each(err.responseJSON, function(fieldName, errors) {
+                    $.each(err.responseJSON.errors, function(fieldName, errors) {
                         var $field = $('#'+fieldName);
                         $field.addClass('error');
                         $field.siblings('.error-message').text(errors[0]);
@@ -352,9 +352,9 @@ Kora.Fields.Show = function() {
                 method: 'POST',
                 data: values,
                 error: function(err) {
-                    if (err.responseJSON[field] !== undefined) {
+                    if (err.responseJSON.errors[field] !== undefined) {
                         $('#'+field).addClass('error');
-                        $('#'+field).siblings('.error-message').text(err.responseJSON[field][0]);
+                        $('#'+field).siblings('.error-message').text(err.responseJSON.errors[field][0]);
                     } else {
                         $('#'+field).removeClass('error');
                         $('#'+field).siblings('.error-message').text('');
