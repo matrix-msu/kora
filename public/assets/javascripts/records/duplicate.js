cec07da1fe018e2f0ae84bf7c99b409cde87b1a9
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-06-26 13:43:30 -0400

    editing border styles, adding js to allow for position: sticky behavior

--- public/assets/javascripts/records/duplicate.js
+++ public/assets/javascripts/records/duplicate.js
@@ -3,3 +3,14 @@ $('.inner-wrap .duplicate-record-special-js .text-input').on('blur', function ()
   var a = $('.inner-wrap .duplicate-record-special-js').children('.text-input').val();
   $('form .duplicate-record-js .text-input').val('' + a + '');
 });
+
+$(window).scroll(function () {
+  console.log($(window).scrollTop())
+  if ($(window).scrollTop() >= 210) {
+    $('.duplicate-record-special-js').addClass('fixed');
+    $('.content-sections').css('padding-top', '148px');
+  } else {
+    $('.duplicate-record-special-js').removeClass('fixed');
+    $('.content-sections').css('padding-top', '');
+  }
+});
