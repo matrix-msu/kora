c8ab3ade980af4b201734fbe6bf62efd93a6d7ee
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-07-26 11:15:46 -0400

    editing smalltext below admin selections, modifying list of users to add to project/form admin list

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -134,7 +134,8 @@ class ProjectController extends Controller {
         if(!\Auth::user()->admin)
             return redirect('projects')->with('k3_global_error', 'not_admin');
 
-        $users = User::pluck('username', 'id')->all();
+        //$users = User::pluck('username', 'id')->all();
+        $users = User::where('id', '!=', 1)->pluck('username', 'id')->all();
         $projectMode = "project_create";
 
         return view('projects.create', compact('users','projectMode'));
