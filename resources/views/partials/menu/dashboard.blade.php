2558781dd8f49b7f3a1a4235d18ffd285ad3c5af
RD <devolde2@msu.edu>
2018-08-01 13:56:56 -0400

    indenting change

--- resources/views/partials/menu/dashboard.blade.php
+++ resources/views/partials/menu/dashboard.blade.php
@@ -45,32 +45,34 @@
 
         <?php $allowed_projects = \Auth::user()->allowedProjects() ?>
         @if(sizeof($allowed_projects) > 1)
-            <li class="link">
-              <a href='#' class="navigation-sub-menu-toggle navigation-sub-menu-toggle-js">
-                <span>Jump to Project</span>
-                <i class="icon sub-menu-icon icon-plus"></i>
-              </a>
-			  
-			  <?php
-			  // Sort projects by name
-			  $name_pid_projects = [];
-			  
-			  foreach ($allowed_projects as $project)
-			  {
-			    $name_pid_projects[$project->pid] = $project->name;
-			  }
-			  
-			  asort($name_pid_projects, SORT_NATURAL | SORT_FLAG_CASE);
-			  ?>
-			  
-              <ul class="navigation-deep-menu navigation-deep-menu-js">
-                @foreach($name_pid_projects as $project_pid => $project_name)
-                  <li class="deep-menu-item">
-                    <a href="{{ url('/projects/'.$project_pid) }}">{{ $project_name }}</a>
-                  </li>
-                @endforeach
-              </ul>
-            </li>
+			@if(\Auth::user()->admin==1)
+		<li class="link">
+			@else
+		<li class="link first">
+			@endif
+           <a href='#' class="navigation-sub-menu-toggle navigation-sub-menu-toggle-js">
+             <span>Jump to Project</span>
+             <i class="icon sub-menu-icon icon-plus"></i>
+           </a>
+		  
+		   <?php
+		   // Sort projects by name
+		   $name_pid_projects = [];
+		   foreach ($allowed_projects as $project)
+		   {
+		     $name_pid_projects[$project->pid] = $project->name;
+		   }
+		   asort($name_pid_projects, SORT_NATURAL | SORT_FLAG_CASE);
+		   ?>
+		  
+           <ul class="navigation-deep-menu navigation-deep-menu-js">
+             @foreach($name_pid_projects as $project_pid => $project_name)
+               <li class="deep-menu-item">
+                 <a href="{{ url('/projects/'.$project_pid) }}">{{ $project_name }}</a>
+               </li>
+             @endforeach
+           </ul>
+         </li>
         @endif
 		
 		@if(\Auth::user()->admin==0)
