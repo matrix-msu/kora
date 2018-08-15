b83cac5f80371ee3208e28621df4aedd949a2190
RD <devolde2@msu.edu>
2018-08-15 14:47:20 -0400

    loading icon styling changes & code cleanup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -52,7 +52,7 @@
         </script>
       @endif
        <script src="{{ config('app.url') }}/assets/javascripts/navigation/breadcrumbs.js"></script>
-	   <div id="preloader" style="display:none;">
+	   <div class="preloader" style="display:none;">
          <div id="loader-background">
 		   <div id="loader">
 		     
