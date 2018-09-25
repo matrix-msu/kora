21ef97c92addd59d0f25ce56b00b554eb1d63bc1
RD <devolde2@msu.edu>
2018-09-25 11:07:24 -0400

    Find email via plaintext token

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Contracts\Hashing;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +40,35 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+	
+	/**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+		// find which hashed token in the database can result from the given plaintext token
+		$app_hasher = app()['hash'];
+		$entries = DB::table('password_resets')->get();
+		
+		$hashed_token = "";
+		foreach ($entries as $entry) {
+			$hash_check = $app_hasher->check($token, $entry->token);
+			
+			if ($hash_check !== null && $hash_check == 1) {
+				$request->email = $entry->email;
+				break;
+			}
+		}
+		
+        return view('auth.passwords.reset')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
     }
 }
