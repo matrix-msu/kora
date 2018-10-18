b6f61548aa8779ca62af3d07c84d7e134d486046
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-10-18 12:18:26 -0400

    adjusting spacing on date option for combofield

--- resources/views/partials/fields/combo/options/date.blade.php
+++ resources/views/partials/fields/combo/options/date.blade.php
@@ -4,7 +4,7 @@
     {!! Form::input('number', 'start_'.$fnum, \App\ComboListField::getComboFieldOption($field,'Start',$fnum), ['class' => 'text-input']) !!}
 </div>
 
-<div class="form-group mt-xl half pl-m">
+<div class="form-group mt-xxl half pl-m">
     {!! Form::label('end_'.$fnum,'End Year: ') !!}
     <span class="error-message"></span>
     {!! Form::input('number', 'end_'.$fnum, \App\ComboListField::getComboFieldOption($field,'End',$fnum), ['class' => 'text-input']) !!}
