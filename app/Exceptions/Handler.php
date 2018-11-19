438282c89c1840c8f1e2e9ebb3cfcf72d625f43e
RD <devolde2@msu.edu>
2018-11-19 13:54:00 -0500

    exception handling & 500 scss file

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -41,12 +41,17 @@ class Handler extends ExceptionHandler {
 	public function render($request, Exception $exception)
 	{
 		if ($this->isHttpException($exception)) {
-			Log::info("isHttpException");
 			switch ($exception->getStatusCode()) {
 				case 404:
 					return response()->view('errors.404', ['install_admin_email' => $install_admin->email], 404);
 					break;
 			}
+		}
+		
+		if ($exception instanceof \ErrorException) {
+			Log::info("ErrorException!");
+			$install_admin = User::where('id','=',1)->first();
+			return response()->view('errors.500', ['install_admin_email' => $install_admin->email], 500);
 		}
 		
 		return parent::render($request, $exception);
