5fd1b6479d741fce05e25ec505e1c30bce65c014
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-11-27 15:46:53 -0500

    showing and hiding field emptystate dynamically rather than on page load

--- resources/views/forms/show.blade.php
+++ resources/views/forms/show.blade.php
@@ -54,7 +54,7 @@
     if (count($page["fields"]) > 0)
   	{
 	  $page_has_fields = true;
-	  break;	
+	  break;
   	}
   }
   ?>
@@ -166,10 +166,9 @@
               <input type="submit" value="Create New Field Here">
             </div>
           </form>
-          @if (!count($page["fields"]) > 0)
-            @include('forms.layout.no-fields')
-          @endif
         @endif
+
+        @include('forms.layout.no-fields')
       </div>
 
       @if(\Auth::user()->canCreateFields($form))
