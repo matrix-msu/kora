f5d6b2170773700ed26cff2d0369d12fcf04ae4f
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2018-11-28 17:30:31 -0500

    adjusting styles of delete-block modal

--- resources/views/partials/dashboard/deleteBlockModal.blade.php
+++ resources/views/partials/dashboard/deleteBlockModal.blade.php
@@ -1,13 +1,13 @@
 <div class="modal modal-js modal-mask delete-block-modal delete-block-modal-js">
     <div class="content small">
         <div class="header">
-            <span class="title">Delete Block?</span>
+            <span class="title delete-block">Delete Block?</span>
             <a href="#" class="modal-toggle modal-toggle-js">
                 <i class="icon icon-cancel"></i>
             </a>
         </div>
         <div class="body">
-			<span class="description">Are you sure you wish to delete this block?</span>
+			<span class="description delete-block">Are you sure you wish to delete this block?</span>
 			<form class="delete-block-form-js" action="">
 				<input type="hidden" name="_token" value="{{ csrf_token() }}">
 				<div class="form-group">
