7738c0d3ae241eeb4b260daac79ae7a6f12e403c
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2019-01-22 13:56:03 -0500

    sorting new array of forms alphabetically

--- resources/views/partials/dashboard/addBlockModalForm.blade.php
+++ resources/views/partials/dashboard/addBlockModalForm.blade.php
@@ -33,8 +33,8 @@
         <span class="error-message"></span>
         <select class="single-select" id="block_form" name="block_form" data-placeholder="Select a Form">
             <option></option>
-            @foreach($userForms as $form)
-                <option value="{{$form->fid}}">{{ \App\Http\Controllers\ProjectController::getProject($form->pid)->name }} - {{$form->name}}</option>
+            @foreach($userForms as $index=>$form)
+                <option value="{{ $index }}">{{ $form }}</option>
             @endforeach
         </select>
     </div>
