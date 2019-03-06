57503acce2d2f1e344df732438949d519ab8e281
Kevin Sutherland <kevin.sutherland@peach.matrix.msu.edu>
2019-03-06 11:47:44 -0500

    adding tooltips to geolocator field on both record-index and record-show pages

--- resources/views/partials/records/display/geolocator.blade.php
+++ resources/views/partials/records/display/geolocator.blade.php
@@ -9,7 +9,7 @@
         @endif
     @endforeach
 @else
-    <div id="map{{$field->flid}}_{{$record->rid}}" class="geolocator-map geolocator-map-js mt-xxs" map-id="{{$field->flid}}_{{$record->rid}}">
+    <div class="sidebar-container"> <!-- div#map has overflow:hidden, therefore we can't add tooltips to div.field-sidebar because they get cropped.  Need containing element with position:relative -->
         <div class="field-sidebar">
             <div class="top">
                 <a target="_blank" href="{{ action('FieldController@singleGeolocator', ['pid' => $field->pid, 'fid' => $field->fid, 'rid' => $record->rid, 'flid' => $field->flid]) }}" class="field-btn external-button-js tooltip" tooltip="Open in New Tab">
@@ -23,21 +23,23 @@
                 </div>
             </div>
         </div>
+        <div id="map{{$field->flid}}_{{$record->rid}}" class="geolocator-map geolocator-map-js mt-xxs" map-id="{{$field->flid}}_{{$record->rid}}">
 
-        @foreach( App\GeolocatorField::locationsToOldFormat($typedField->locations()->get()) as $location)
-            <span class="geolocator-location-js hidden" loc-desc="{{explode('[Desc]',$location)[1]}}"
-                  loc-x="{{explode(',', explode('[LatLon]',$location)[1])[0]}}"
-                  loc-y="{{explode(',', explode('[LatLon]',$location)[1])[1]}}"></span>
-        @endforeach
+            @foreach( App\GeolocatorField::locationsToOldFormat($typedField->locations()->get()) as $location)
+                <span class="geolocator-location-js hidden" loc-desc="{{explode('[Desc]',$location)[1]}}"
+                    loc-x="{{explode(',', explode('[LatLon]',$location)[1])[0]}}"
+                    loc-y="{{explode(',', explode('[LatLon]',$location)[1])[1]}}"></span>
+            @endforeach
 
-        <div class="full-screen-modal modal modal-js modal-mask geolocator-map-modal geolocator-map-modal-js">
-            <div class="content">
-                <div class="body">
-                    <a href="#" class="modal-toggle modal-toggle-js">
-                        <i class="icon icon-cancel"></i>
-                    </a>
+            <div class="full-screen-modal modal modal-js modal-mask geolocator-map-modal geolocator-map-modal-js">
+                <div class="content">
+                    <div class="body">
+                        <a href="#" class="modal-toggle modal-toggle-js">
+                            <i class="icon icon-cancel"></i>
+                        </a>
 
-                    <div id="modalmap{{$field->flid}}_{{$record->rid}}" class="geolocator-modal-map geolocator-modal-map-js"></div>
+                        <div id="modalmap{{$field->flid}}_{{$record->rid}}" class="geolocator-modal-map geolocator-modal-map-js"></div>
+                    </div>
                 </div>
             </div>
         </div>
