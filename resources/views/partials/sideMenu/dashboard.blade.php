f2da5decd1ea3395c89f9d99122b1b8ff3979a85
Super-User <anthon70@msu.edu>
2019-06-24 17:57:05 +0000

    Another log fix

--- resources/views/partials/sideMenu/dashboard.blade.php
+++ resources/views/partials/sideMenu/dashboard.blade.php
@@ -30,7 +30,7 @@
         </li>
       @endif
 
-      <?php $allowed_projects = \Auth::user()->allowedProjects() ?>
+      @php $allowed_projects = !is_null(\Auth::user()) ? \Auth::user()->allowedProjects() : array(); @endphp
       @if(sizeof($allowed_projects) > 1)
         <li class="content-link content-link-js" id="project-submenu">
           <a href='#' class="drawer-sub-menu-toggle drawer-sub-menu-toggle-js" data-toggle="dropdown">
@@ -38,17 +38,16 @@
             <i class="icon icon-plus"></i>
           </a>
 		  
-		  <?php
-		  // Sort projects by name
-		  $name_pid_projects = [];
-		  
-		  foreach ($allowed_projects as $project)
-		  {
-		    $name_pid_projects[$project->id] = $project->name;
-		  }
-		  
-		  asort($name_pid_projects, SORT_NATURAL | SORT_FLAG_CASE);
-		  ?>
+		  @php
+            // Sort projects by name
+            $name_pid_projects = [];
+
+            foreach ($allowed_projects as $project) {
+              $name_pid_projects[$project->id] = $project->name;
+            }
+
+            asort($name_pid_projects, SORT_NATURAL | SORT_FLAG_CASE);
+		  @endphp
 
           <ul class="drawer-deep-menu drawer-deep-menu-js">
             @foreach($name_pid_projects as $project_pid => $project_name)
