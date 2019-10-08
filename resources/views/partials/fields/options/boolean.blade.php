647188cbc655fa1c8d1291443710fd17339f2304
Super-User <anthon70@msu.edu>
2019-10-08 18:18:32 +0000

    Fix for boolean fields that don't need regex

--- resources/views/partials/fields/options/boolean.blade.php
+++ resources/views/partials/fields/options/boolean.blade.php
@@ -1,10 +1,5 @@
 @extends('fields.show')
 
-@section('presetModal')
-    @include('partials.fields.fieldValuePresetModals.addRegexPresetModal', ['presets' => $presets])
-    @include('partials.fields.fieldValuePresetModals.createRegexPresetModal')
-@stop
-
 @section('fieldOptions')
     @include('partials.fields.options.defaults.boolean')
 @stop
