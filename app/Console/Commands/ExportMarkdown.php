bd7d1e604699ca4b2525760041e1ccd37d7e1113
Anthony Donofrio <anthon70@msu.edu>
2025-10-17 14:30:25 +0000

    Completed system for exporting markdown records

--- app/Console/Commands/ExportMarkdown.php
+++ app/Console/Commands/ExportMarkdown.php
@@ -47,6 +47,50 @@ class ExportMarkdown extends Command
         $filters = ["revAssoc" => true, "meta" => false, "fields" => 'ALL', "altNames" => false, "assoc" => false,
             "data" => true, "sort" => null, "count" => null, "index" => null];
 
-        var_dump($form->getRecordsForExportMarkdown($filters, null, $this->argument('title'), $this->argument('longform')));
+        $markdownData = $form->getRecordsForExportMarkdown($filters, null, $this->argument('title'), $this->argument('longform'));
+        $tmpDir = uniqid();
+
+        foreach($markdownData as $filename => $record) {
+            $formattedName = $this->formatValidMarkdownFilename($filename);
+
+            switch(config('filesystems.kora_storage')) {
+                case FileTypeField::_LaravelStorage:
+                    //Make folder
+                    $newPath = storage_path('app/tmpFiles/' . $tmpDir);
+                    if(!file_exists($newPath))
+                        mkdir($newPath, 0775, true);
+
+                    file_put_contents($newPath . '/' . $formattedName, $record);
+                    break;
+                case FileTypeField::_JoyentManta:
+                    //TODO::MANTA
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        $this->info("Markdown files exported successfully. Stored at: $newPath\n");
+    }
+
+    public function formatValidMarkdownFilename($string, $maxLength = 255) {
+        // Replace spaces with underscores
+        $filename = str_replace(' ', '_', $string);
+
+        // Remove characters not allowed in filenames
+        $filename = preg_replace('/[^A-Za-z0-9_\-\.]/', '', $filename);
+
+        // Remove multiple underscores or dots
+        $filename = preg_replace('/[_\.]{2,}/', '_', $filename);
+
+        // Trim to max length
+        $filename = substr($filename, 0, $maxLength);
+
+        // Ensure it's not empty
+        if (empty($filename)) {
+            $filename = 'default_filename';
+        }
+
+        return $filename.".md";
     }
 }
